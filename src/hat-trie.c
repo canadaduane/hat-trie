c118bb8209778e4071b6ffb5a04c6fd55ce33e8c
Zete Lui <mas@k.ed>
2013-02-03 11:12:15 +0800

    clip prefix in key retrieving

--- src/hat-trie.c
+++ src/hat-trie.c
@@ -689,8 +689,8 @@ const char* hattrie_iter_key(hattrie_iter_t* i, size_t* len)
     memcpy(i->key + i->level, subkey, sublen);
     i->key[i->level + sublen] = '\0';
 
-    *len = i->level + sublen;
-    return i->key;
+    *len = i->level + sublen - i->prefix_len;
+    return i->key + i->prefix_len;
 }
 
 
